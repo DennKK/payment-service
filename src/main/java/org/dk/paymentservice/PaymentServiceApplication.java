0403b4327463e03e77d80cf0a6323bca88fedaaa
Denis Karpov <karpovdenis.al@gmail.com>
2025-05-05 17:28:43 +0300

    load env variables from .env using java-dotenv

--- src/main/java/org/dk/paymentservice/PaymentServiceApplication.java
+++ src/main/java/org/dk/paymentservice/PaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package org.dk.paymentservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
+        Dotenv.load().entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 

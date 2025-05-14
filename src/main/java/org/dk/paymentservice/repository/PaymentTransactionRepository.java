8be7a1ef7aa68bd86f83d2a82e8951f8a3901873
Denis Karpov <karpovdenis.al@gmail.com>
2025-05-14 19:03:25 +0300

    Add method with custom query

--- src/main/java/org/dk/paymentservice/repository/PaymentTransactionRepository.java
+++ src/main/java/org/dk/paymentservice/repository/PaymentTransactionRepository.java
@@ -1,9 +1,14 @@
 package org.dk.paymentservice.repository;
 
+import java.util.Optional;
 import org.dk.paymentservice.model.entity.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Long> {
+    @Query("SELECT pt FROM PaymentTransaction pt LEFT JOIN FETCH pt.refunds WHERE pt.id = :id")
+    Optional<PaymentTransaction> findByIdWithRefunds(@Param("id") Long id);
 }

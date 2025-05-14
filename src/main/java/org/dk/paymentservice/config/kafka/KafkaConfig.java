c9073ae84d48b46910fa0252e836816701074e67
Denis Karpov <karpovdenis.al@gmail.com>
2025-05-14 17:23:21 +0300

    Update kafka configuration

--- src/main/java/org/dk/paymentservice/config/kafka/KafkaConfig.java
+++ src/main/java/org/dk/paymentservice/config/kafka/KafkaConfig.java
@@ -1,10 +1,11 @@
 package org.dk.paymentservice.config.kafka;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -14,40 +15,37 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class KafkaConfig {
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory(
-            @Value("${spring.kafka.bootstrapServers[0]}") final String bootstrapServers) {
+    public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-service-group-id");
-        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-            final ConsumerFactory<String, String> consumerFactory) {
+        final ConsumerFactory<String, String> consumerFactory
+    ) {
         final ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+            new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
 
         return factory;
     }
 
     @Bean
-    public ProducerFactory<String, String> producerFactory(
-            @Value("${spring.kafka.bootstrapServers[0]}") final String bootstrapServers) {
+    public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
@@ -56,7 +54,9 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(final ProducerFactory<String, String> producerFactory) {
+    public KafkaTemplate<String, String> kafkaTemplate(
+        final ProducerFactory<String, String> producerFactory
+    ) {
         return new KafkaTemplate<>(producerFactory);
     }
 }
--- src/main/java/org/dk/paymentservice/config/kafka/KafkaTopicConfig.java
+++ src/main/java/org/dk/paymentservice/config/kafka/KafkaTopicConfig.java
@@ -1,34 +1,32 @@
 package org.dk.paymentservice.config.kafka;
 
-import lombok.RequiredArgsConstructor;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Configuration
-@RequiredArgsConstructor
 public class KafkaTopicConfig {
-    private final KafkaProperties kafkaProperties;
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String COMMAND_TOPIC = "payment-command";
+    private static final String RESULT_TOPIC = "payment-command-result";
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,
-                String.join(",", kafkaProperties.getBootstrapServers()));
-        configs.putAll(kafkaProperties.getAdminProperties());
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         return new KafkaAdmin(configs);
     }
 
     @Bean
     public List<NewTopic> createTopics() {
-        return kafkaProperties.getTopics().stream()
-                .map(topic -> new NewTopic(topic.getName(), topic.getPartitions(), topic.getReplicationFactor()))
-                .toList();
+        return List.of(
+            new NewTopic(COMMAND_TOPIC, 1, (short) 1),
+            new NewTopic(RESULT_TOPIC, 1, (short) 1)
+        );
     }
 }

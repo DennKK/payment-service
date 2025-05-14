5f3e81fc59fd2df18dd079413377b659ab02a95c
Denis Karpov <karpovdenis.al@gmail.com>
2025-05-14 17:19:14 +0300

    Add method to convert object to json string

--- src/main/java/org/dk/paymentservice/util/JsonConverter.java
+++ src/main/java/org/dk/paymentservice/util/JsonConverter.java
@@ -2,22 +2,31 @@ package org.dk.paymentservice.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.experimental.UtilityClass;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@UtilityClass
+@RequiredArgsConstructor
 public class JsonConverter {
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     public <T> T toObject(String json, Class<T> clazz) {
         try {
-            return mapper.readValue(json, clazz);
-        } catch (JsonProcessingException ex) {
-            log.error("Json deserializing exception: {}", ex.getMessage());
-            throw new RuntimeException(ex);
+            return objectMapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            log.error("Json deserializing exception: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            log.error("Json serializing exception: {}", e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
